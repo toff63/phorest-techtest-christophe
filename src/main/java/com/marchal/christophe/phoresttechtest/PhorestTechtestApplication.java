45cefed10afbd18ca2d05d6e6043d373c7cbc89d
Christophe Marchal <christophe@getonce.com>
2023-01-27 15:38:37 +0000

    Project generated using  https://start.spring.io

---
+++ src/main/java/com/marchal/christophe/phoresttechtest/PhorestTechtestApplication.java
@@ -0,0 +1,13 @@
+package com.marchal.christophe.phoresttechtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhorestTechtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhorestTechtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marchal/christophe/phoresttechtest/PhorestTechtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marchal.christophe.phoresttechtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhorestTechtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
