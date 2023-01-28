65b513a10d66361c9a064c79ad4977d0c2bd21bc
Christophe Marchal <christophe@getonce.com>
2023-01-28 17:36:35 +0000

    Rename main class

--- src/main/java/com/marchal/christophe/phoresttechtest/PhorestTechTestApplication.java
+++ src/main/java/com/marchal/christophe/phoresttechtest/PhorestTechTestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PhorestTechtestApplication {
+public class PhorestTechTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhorestTechtestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PhorestTechTestApplication.class, args);
+    }
 
 }
--- src/test/java/com/marchal/christophe/phoresttechtest/PhorestTechTestApplicationTests.java
+++ src/test/java/com/marchal/christophe/phoresttechtest/PhorestTechTestApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PhorestTechtestApplicationTests {
+class PhorestTechTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
